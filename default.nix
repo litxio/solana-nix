a9cc959a84b9ca6dee52e446e9e09cd4ccb176a5
Richard Warfield <richard@litx.io>
2023-11-15 10:34:23 +0700

    Initial commit

---
+++ default.nix
@@ -0,0 +1,80 @@
+{ src,
+  version,
+  lib,
+  rustPlatform,
+  rustfmt,
+  fetchurl,
+  pkg-config,
+  protobuf,
+  udev,
+  openssl,
+  llvmPackages,
+  perl,
+  clang,
+  perf-libs,
+  extraTools ? [] }:
+
+rustPlatform.buildRustPackage rec {
+  pname = "solana";
+
+  inherit src version;
+
+  doCheck = false;
+
+  #buildType = "debug";
+  #dontStrip = true;
+
+  nativeBuildInputs = [
+    pkg-config
+    protobuf
+    perl
+    clang
+    rustfmt
+  ] ++ extraTools;
+
+  # Needed so bindgen can find libclang.so
+  LIBCLANG_PATH="${llvmPackages.libclang.lib}/lib";
+
+  buildInputs = [
+    udev
+    llvmPackages.clang-unwrapped.lib
+    openssl
+    protobuf
+    clang
+    rustfmt
+  ];
+
+  postInstall = ''
+    cp -r ${perf-libs.out} $out/bin/perf-libs
+  '';
+
+  #sourceRoot = ".";
+  cargoLock = {
+    lockFile = "${src}/Cargo.lock";
+    outputHashes = {
+      "crossbeam-epoch-0.9.5" = "sha256-Jf0RarsgJiXiZ+ddy0vp4jQ59J9m0k3sgXhWhCdhgws=";
+      "ntapi-0.3.7" = "sha256-G6ZCsa3GWiI/FeGKiK9TWkmTxen7nwpXvm5FtjNtjWU=";
+    };
+  };
+
+  #autoPatchelfIgnoreMissingDeps=true;
+
+  #installPhase = ''
+  #  #cp -r solana-release/bin/* $out/bin/
+  #  for f in solana-release/bin/*; do
+  #    if [[ -f "$f" && -x "$f" ]]
+  #    then
+  #      echo "Installing $f" > /dev/stderr
+  #      install -m755 -D "$f" "$out/bin/$(basename $f)"
+  #    else
+  #      cp -r "$f" $out/bin
+  #    fi
+  #  done
+  #'';
+
+  # meta = with lib; {
+  #   homepage = "https://studio-link.com";
+  #   description = "Voip transfer";
+  #   platforms = platforms.linux;
+  # };
+}
---
+++ flake.nix
@@ -0,0 +1,108 @@
+{
+  description = "Solana validators (Labs and Jito)";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.fenix = {
+    url = "github:nix-community/fenix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  inputs.solana-src-1_16_19 = {
+    url = "git+https://github.com/solana-labs/solana.git?ref=refs/tags/v1.16.19";
+    flake = false;
+  };
+
+  inputs.jito-solana-src-1_16_18 = {
+    url = "git+https://github.com/jito-foundation/jito-solana.git?ref=refs/tags/v1.16.18-jito&submodules=1";
+    flake = false;
+  };
+
+
+  outputs = inputs@{ self, nixpkgs, fenix, ... }:
+    let pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+        bins = [ "solana"
+                 "solana-bench-tps"
+                 "solana-faucet"
+                 "solana-gossip"
+                 "solana-install"
+                 "solana-keygen"
+                 "solana-ledger-tool"
+                 "solana-log-analyzer"
+                 "solana-net-shaper"
+                 #"solana-sys-tuner"
+                 "solana-validator"
+                 "rbpf-cli"
+                 "cargo-build-bpf"
+                 "cargo-build-sbf"
+                 "cargo-test-bpf"
+                 "cargo-test-sbf"
+                 "solana-dos"
+                 "solana-install-init"
+                 "solana-stake-accounts"
+                 "solana-test-validator"
+                 "solana-tokens"
+                 "solana-watchtower"
+                 "solana-genesis" ];
+
+        toolchain_1_69_0 = #fenix.packages.x86_64-linux.latest;
+          fenix.packages.x86_64-linux.toolchainOf {
+            channel = "1.69.0";
+            sha256 = "sha256-eMJethw5ZLrJHmoN2/l0bIyQjoTX1NsvalWSscTixpI=";
+          };
+
+        build = version: src: toolchain:
+          let
+            rustPlatform = pkgs.makeRustPlatform {
+              inherit (toolchain) cargo rustc rustfmt rust-src;
+            };
+          in pkgs.callPackage ./default.nix {
+            inherit version;
+            inherit src;
+            inherit rustPlatform;
+            inherit (toolchain) rustfmt;
+            inherit perf-libs;
+            extraTools = [fenix.packages.x86_64-linux.rust-analyzer
+                          (toolchain.withComponents [
+                            "cargo" "rustc" "rust-src" "rustfmt" "clippy"
+                          ])
+                         ];
+          };
+
+        apps =
+          # Plain names for plain solana
+          (pkgs.lib.genAttrs bins (name : {
+            type = "app";
+            program = "${self.packages.x86_64-linux.solana}/bin/${name}";
+          }))
+          //
+          # Prefix names for Jito bins with "jito-"
+          (builtins.listToAttrs (builtins.map (name: {
+            name = "jito-${name}";
+            value = {
+              type = "app";
+              program = "${self.packages.x86_64-linux.jito-solana}/bin/${name}";
+            };
+          }) bins));
+
+        perf-libs-version = "v0.19.3";
+        perf-libs = pkgs.fetchzip {
+          url = "https://github.com/solana-labs/solana-perf-libs/releases/download/${perf-libs-version}/solana-perf.tgz";
+          sha256 = "sha256-fvbz2uqVmThAUQwyuDYaF7ajmMUbbatE9LMVHDKCYWY=";
+          stripRoot = false;
+        };
+    in
+      {
+        packages.x86_64-linux.solana-1_16_19 =
+          build "1.16.19" inputs.solana-src-1_16_19 toolchain_1_69_0;
+        packages.x86_64-linux.solana = self.packages.x86_64-linux.solana-1_16_19;
+
+        packages.x86_64-linux.jito-solana-1_16_18 =
+          build "1.16.18-jito" inputs.jito-solana-src-1_16_18 toolchain_1_69_0;
+        packages.x86_64-linux.jito-solana = self.packages.x86_64-linux.jito-solana-1_16_18;
+
+        #packages.x86_64-linux.default = self.packages.x86_64-linux.jito-solana;
+
+        apps.x86_64-linux = apps;
+      };
+}
