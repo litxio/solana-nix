e2d0a8f915f205558e902e1229977a0497a6069c
Richard Warfield <richard@litx.io>
2024-05-22 11:37:37 +0700

    1.17.34

--- flake.nix
+++ flake.nix
@@ -90,6 +90,11 @@
     flake = false;
   };
 
+  inputs.solana-src-1_17_34 = {
+    url = "git+https://github.com/solana-labs/solana.git?ref=refs/tags/v1.17.34";
+    flake = false;
+  };
+
   # Jito Solana
 
   inputs.jito-solana-src-1_16_18 = {
@@ -170,6 +175,11 @@
 
   inputs.jito-solana-src-1_17_33 = {
     url = "git+https://github.com/jito-foundation/jito-solana.git?ref=refs/tags/v1.17.33-jito&submodules=1";
+    flake = false;
+  };
+
+  inputs.jito-solana-src-1_17_34 = {
+    url = "git+https://github.com/jito-foundation/jito-solana.git?ref=refs/tags/v1.17.34-jito&submodules=1";
     flake = false;
   };
 
@@ -308,7 +318,9 @@
             build "1.17.31" arch inputs.solana-src-1_17_31 toolchain_1_73_0 outputHashes117;
           solana-1_17_33 = arch:
             build "1.17.33" arch inputs.solana-src-1_17_33 toolchain_1_73_0 outputHashes117;
-          solana = arch: self.packages.x86_64-linux."solana-1_17_33/${arch}";
+          solana-1_17_34 = arch:
+            build "1.17.34" arch inputs.solana-src-1_17_34 toolchain_1_73_0 outputHashes117;
+          solana = arch: self.packages.x86_64-linux."solana-1_17_34/${arch}";
 
           jito-solana-1_16_18 = arch:
             build "1.16.18-jito" arch inputs.jito-solana-src-1_16_18 toolchain_1_69_0 outputHashes116;
@@ -340,7 +352,9 @@
             build "1.17.31" arch inputs.jito-solana-src-1_17_31 toolchain_1_73_0 outputHashes117;
           jito-solana-1_17_33 = arch:
             build "1.17.33" arch inputs.jito-solana-src-1_17_33 toolchain_1_73_0 outputHashes117;
-          jito-solana = arch: self.packages.x86_64-linux."jito-solana-1_17_33/${arch}";
+          jito-solana-1_17_34 = arch:
+            build "1.17.34" arch inputs.jito-solana-src-1_17_34 toolchain_1_73_0 outputHashes117;
+          jito-solana = arch: self.packages.x86_64-linux."jito-solana-1_17_34/${arch}";
         };
 
         #packages.x86_64-linux.default = self.packages.x86_64-linux.jito-solana;
